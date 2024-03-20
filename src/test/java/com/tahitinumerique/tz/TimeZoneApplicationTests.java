c1d2eec4f426d024c7cc149ac81955f29142093e
tauahako <tauahako@gmail.com>
2024-03-20 10:34:45 -1000

    feat: initial commit

---
+++ src/test/java/com/tahitinumerique/tz/TimeZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tahitinumerique.tz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
